895695c7f9f36269d37bb8fae63051851cc3a1a4
Filip Maj <fmaj@slack-corp.com>
2023-05-08 14:30:50 -0400

    Remove no longer needed `@ts-ignore` directives. Fixes #8
    Use trigger data and type enumerations in trigger definition. Dont hardcode workflow callback id in trigger definition.

--- functions/send_announcement/handler.ts
+++ functions/send_announcement/handler.ts
@@ -57,7 +57,6 @@ export default SlackFunction(
         typeof DraftDatastore.definition
       >({
         datastore: DraftDatastore.name,
-        // @ts-ignore expected fix in future release - otherwise missing non-required items throw type error
         item: {
           id: inputs.draft_id,
           status: DraftStatus.Sent,
